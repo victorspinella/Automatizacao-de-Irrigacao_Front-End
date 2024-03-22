1fe4c40cd2812c4987f3ce2c47211355f3d08e06
Victor Spinella Moreira <victorspinellamoreira@gmail.com>
2024-03-21 22:55:30 -0300

    ajustes login firebase

--- front_dsw/app.vue
+++ front_dsw/app.vue
@@ -1,7 +1,26 @@
 <template>
-  <div>
+  <nav>
+    <router-link to="/"> Home </router-link> |
+    <router-link to="/status"> Status </router-link> |
+    <router-link to="/register"> Register </router-link> |
+    <router-link to="/sign-in"> Login </router-link> |
+    <button @click="handleSignOut">Sign out</button>
+  </nav>
+  <router-view />
+  <!-- <div>
     <NuxtLayout>
       <NuxtPage/>
     </NuxtLayout>
-  </div>
+  </div> -->
 </template>
+
+<script setup>
+import { onMounted, ref } from "vue";
+
+const is LoggedIn = ref(false);
+
+const handleSignOut = () => {
+
+};
+
+</script>
